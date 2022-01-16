fea008aa1d0da7157b82160a09c5199a380e6dfc
fortune <omorodion.fortune@gmail.com>
2022-01-16 21:08:17 +0100

    modified App.js and VideoDetail component

--- video/src/components/VideoDetail.js
+++ video/src/components/VideoDetail.js
@@ -2,7 +2,24 @@ import React from "react";
 
 
 const VideoDetail = ({video}) =>{
-    return <div>{video.snippet.title}</div>
+    if (!video){
+        return <div>loading...</div>
+    };
+    const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
+
+    return (
+    <div>
+        <div className="ui embed">
+         <iframe src={VideoSrc}/>
+        </div>
+        <div className="ui segment">
+        <h4 className="ui header">
+        {video.snippet.title}
+        </h4>
+        <p>{video.snippet.description}</p>
+        </div>
+    </div>);
 
 };
 
